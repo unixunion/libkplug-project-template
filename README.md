4264fa66572b9f52242e41eb8710b5f8df583c4a
Kegan Holtzhausen <Kegan.Holtzhausen@kindredgroup.com>
2019-02-18 10:29:26 +0100

    config file location arg

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 
 Contains the source top level modules, and "binscripts" which are mapped to shell commands in setup.py
 
+## config.yaml
+
+The libkplug / libksettings example yaml config is loaded from the 'cwd'. It declares which plugins to load,
+along with any other generic key:value properties required by the application.
+
 ## dev run
 
     python ./src/hello_world_binscripts/run.py
--- src/hello_world_binscripts/run.py
+++ src/hello_world_binscripts/run.py
@@ -2,29 +2,39 @@
 
 __author__ = 'keghol'
 
-import os
 import sys
 import logging
+import argparse
 
 # path modification to be able to run example against src, otherwise wont be able to resolve
-#sys.path.insert(1, os.path.join(sys.path[0], '../src'))
+# sys.path.insert(1, os.path.join(sys.path[0], '../src'))
+
+# import libkplug before settings, but after logging
+import libkplug
+from libksettings import KSettings
+
 
 # initialize the logger before kplug
 logging.basicConfig(format='%(filename)s:%(lineno)s %(levelname)s %(message)s', stream=sys.stdout)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 logging.getLogger().setLevel(logging.INFO)
 
-# import libkplug before settings, but after logging
-import libkplug
-from libksettings import KSettings
-
-# initialize settings with some default values
-settings = KSettings(MY_HELLO_WORLD_CLASS='HelloWorldPlugin', PLUGINS=['hello_world.plugins.plugin_helloworld'])
-
 
 def main():
-
     logging.info("Starting up")
+
+    parser = argparse.ArgumentParser(description='Hello World libkplug example project.')
+    parser.add_argument('--config', type=str,
+                        help='an integer for the accumulator')
+
+    args = parser.parse_args()
+
+    if not args.config:
+        args.config = "config.yaml"
+
+    # initialize settings with some default values
+    settings = KSettings(config_filename=args.config, MY_HELLO_WORLD_CLASS='HelloWorldPlugin',
+                         PLUGINS=['hello_world.plugins.plugin_helloworld'])
 
     # get the class of the plugin
     clazz = libkplug.plugin_registry(settings.MY_HELLO_WORLD_CLASS)
@@ -34,6 +44,7 @@ def main():
 
     while True:
         pass
+
 
 if __name__ == "__main__":
     # execute only if run as a script
